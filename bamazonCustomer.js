411d3b50e58999acfe6625c1c1157c3098efed4f
Bryan Magers <bmagers@users.noreply.github.com>
2018-10-31 16:42:00 -0700

    table and prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,13 +9,39 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+function prompt() {
+  var promptName = {
+    name: "buyName",
+    message: "Please enter the ID of the product you'd like to buy:"
+  };
+  
+  var promptQuantity = {
+    name: "buyQuantity",
+    message: "Please enter the quantity you'd like to buy:"
+  };
+  
+  inquirer.prompt([
+    promptName,
+    promptQuantity
+  ]).then(function(user) {
+    // check if there's enough
+  // if not, "Insufficient quantity!"
+  // if so, update quantity remaining
+  // show customer total cost of purchase
+  });
+}
+
 // display items for sale
-// ids, names, prices
-
-// prompt for ID of product to buy
-// prompt for quantity
-
-// check if there's enough
-// if not, "Insufficient quantity!"
-// if so, update quantity remaining
-// show customer total cost of purchase
+connection.query("SELECT * FROM products", function(err, res) {
+  if (err) throw err;
+  var table = new Table({
+    head: ["ID", "Name", "Price"],
+    colWidths: [6, 40, 20]
+  });
+  res.forEach(function(item, index) {
+    table.push([res[index].item_id, res[index].product_name, res[index].price]);
+  });
+  console.log(table.toString());
+  prompt();
+  connection.end();
+});
